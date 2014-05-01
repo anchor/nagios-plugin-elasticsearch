8152206da5b423115b344e6d70bd69f508a53eb3
Cal Paterson <cal@calpaterson.com>
2014-05-01 15:55:28 +0100

    Add basic setuptools packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+setup(
+    name="nagios-plugin-elasticsearch",
+    verion="1.0",
+    packages=find_packages(),
+    url="https://github.com/anchor/nagios-plugin-elasticsearch",
+    scripts=["check_elasticsearch"],
+    install_requires=[str(req.req) for req in
+                          parse_requirements("requirements.txt")],
+    include_package_data=True
+)
