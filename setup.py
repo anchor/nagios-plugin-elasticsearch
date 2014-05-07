bfbb235db1313d1d9da0ad462c6cbf8b8c0e27be
Sharif Olorin <sio@tesser.org>
2014-05-07 11:16:30 +1000

    Fix typos in setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from pip.req import parse_requirements
 
 setup(
     name="nagios-plugin-elasticsearch",
-    verion="1.0",
+    version="1.0",
     packages=find_packages(),
     url="https://github.com/anchor/nagios-plugin-elasticsearch",
     scripts=["check_elasticsearch"],
-    licence="MIT",
+    license="MIT",
     install_requires=[str(req.req) for req in
                           parse_requirements("requirements.txt")],
     include_package_data=True
