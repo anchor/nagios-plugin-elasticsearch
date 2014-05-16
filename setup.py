e4e9f529b8cdc51cafa42bc3af9bb197636b29f4
Sharif Olorin <sio@tesser.org>
2014-05-16 11:00:56 +1000

    Add description to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from pip.req import parse_requirements
 
 setup(
     name="nagios-plugin-elasticsearch",
+    description="An ElasticSearch availability and performance monitoring plugin for Nagios.",
     version="1.0.1",
     packages=find_packages(),
     url="https://github.com/anchor/nagios-plugin-elasticsearch",
