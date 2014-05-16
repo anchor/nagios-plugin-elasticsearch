f85add9b38c500a4c7ab7189623c9f4245ff59e4
Sharif Olorin <sio@tesser.org>
2014-05-16 10:40:09 +1000

    Minor version bump to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pip.req import parse_requirements
 
 setup(
     name="nagios-plugin-elasticsearch",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     url="https://github.com/anchor/nagios-plugin-elasticsearch",
     maintainer="Sharif Olorin",
