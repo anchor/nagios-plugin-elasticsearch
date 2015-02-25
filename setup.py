6139af792ffb5b67a165eaa0fa7b76f298171223
Sharif Olorin <sio@tesser.org>
2015-02-25 23:36:33 +0000

    New functionality should have been a minor version bump, not patch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pip.req import parse_requirements
 setup(
     name="nagios-plugin-elasticsearch",
     description="An ElasticSearch availability and performance monitoring plugin for Nagios.",
-    version="1.0.3",
+    version="1.1.0",
     packages=find_packages(),
     url="https://github.com/anchor/nagios-plugin-elasticsearch",
     maintainer="Sharif Olorin",
@@ -19,7 +19,7 @@ setup(
       'nagioscheck==0.1.6',
       'simplejson>=3.4.0',
       'setuptools>=3.4.0',
-      'pip>=1.4' 
+      'pip>=1.4'
     ],
     include_package_data=True
 )
